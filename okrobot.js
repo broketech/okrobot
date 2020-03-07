731f2b30e066a1d0770dbfa710fa9c350ba21896
broketech <broketech@randomactual.space>
2020-03-07 14:01:07 -0800

    reply to instead of crafting new tweet now

--- okrobot.js
+++ okrobot.js
@@ -9,6 +9,7 @@ var options = {
   nick: conf.robot.screen_name
 };
 var stream = T.stream('statuses/filter', { track: options.nick });
+//console.log('starting okrobot...')
 stream.on('tweet', function(tweet){
   console.log(tweet.user.screen_name+": "+tweet.text)
   var arg = tweet.text.split(' ');
@@ -16,7 +17,7 @@ stream.on('tweet', function(tweet){
   if((arg[1] == 'markov') ){ // markov request
     TT.twitterHistory(arg[2], function(tdata){
     var mdata = M.genTweet(tdata);
-      T.post('statuses/update', { status: "@"+tweet.user.screen_name+" "+mdata }, function(err, data, response) {
+      T.post('statuses/update', { status: "@"+tweet.user.screen_name+" "+mdata, in_reply_to_status_id: tweet.id_str }, function(err, data, response) {
         console.log("status: @"+tweet.user.screen_name+" "+mdata)
         return;
       });
@@ -27,14 +28,15 @@ stream.on('tweet', function(tweet){
     arg.shift();
     TT.twitterSearch(arg.join(' OR '), function(tdata){
     var mdata = M.genTweet(tdata);
-      T.post('statuses/update', { status: "@"+tweet.user.screen_name+" "+mdata }, function(err, data, response) {
+      T.post('statuses/update', { status: "@"+tweet.user.screen_name+" "+mdata, in_reply_to_status_id: tweet.id_str }, function(err, data, response) {
         console.log("status: @"+tweet.user.screen_name+" "+mdata)
         return;
       });
     })
   }else if((arg[1] == 'scrubdaddy') && (tweet.user.screen_name == 'cheeseanddope')){ // scrub tweets
+    console.log(tweet.user.screen_name + " requested scrub")
     TT.scrubDaddy();
-    T.post('statuses/update', { status: "@"+tweet.user.screen_name+" "+ 'scrubbing tweets' }, function(err, data, response) {
+    T.post('statuses/update', { status: "@"+tweet.user.screen_name+" "+ 'scrubbing tweets daddy uwu' }, function(err, data, response) {
       console.log("status: @"+tweet.user.screen_name+" "+'scrubbing tweets')
       return;
     });
